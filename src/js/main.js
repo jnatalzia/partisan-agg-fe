4a5e8bfa010126535a3fce351debba4c1e03df99
Joe Natalzia <jnatalzia@gmail.com>
2019-05-19 15:05:55 -0400

    Color coding when read

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import { fetchArticles } from './utils/resource_fetcher';
 import { attemptURIDecode, removeHTMLTags } from './utils/formatter';
+import { setArticleRead, getReadArticles } from './utils/storage';
 
 const lastArticleIDStorage = 'lastArticleID';
 
@@ -8,12 +9,14 @@ function checkScroll() {
 
   if (laID) {
     let el = document.querySelector('[data-id="' + laID + '"');
+    localStorage.removeItem(lastArticleIDStorage)
     if (el) {
       window.scrollTo(0, el.offsetTop);
-      localStorage.removeItem(lastArticleIDStorage)
     }
   }
 }
+
+let articlesRead = getReadArticles();
 
 window.addEventListener('load', () => {
   let articleContainer = document.createElement('div');
@@ -21,7 +24,6 @@ window.addEventListener('load', () => {
 
   fetchArticles().then((res) => {
     const articleTemplate = document.querySelector('.js-article-template');
-  
     res.items.forEach(article => {
       let el = articleTemplate.cloneNode(true);
       // TODO: pick the right thumb
@@ -36,7 +38,8 @@ window.addEventListener('load', () => {
       el.querySelector('.js-author').innerText = article.org_name;
       let leanEl = el.querySelector('.js-partisan-lean')
       leanEl.innerText = article.partisan_lean;
-      leanEl.classList.add(`article__partisan-lean--${article.partisan_lean.toLowerCase()}`)
+      leanEl.classList.add(`article__partisan-lean--${article.partisan_lean.toLowerCase()}`);
+      el.classList.add(`article--${article.partisan_lean.toLowerCase()}`);
 
       let formattedPreview = removeHTMLTags(attemptURIDecode(article.article_preview));
       el.querySelector('.js-description').innerText = formattedPreview;
@@ -44,9 +47,15 @@ window.addEventListener('load', () => {
       el.setAttribute('href', article.article_url);
       el.setAttribute('data-id', article.id);
 
+      if (articlesRead[article.id]) {
+        el.classList.add('article--read')
+      }
+
       el.addEventListener('click', e => {
         e.preventDefault();
-        localStorage.setItem(lastArticleIDStorage, e.currentTarget.getAttribute('data-id'));
+        let articleID = e.currentTarget.getAttribute('data-id');
+        setArticleRead(articleID);
+        localStorage.setItem(lastArticleIDStorage, articleID);
         window.location.href = e.currentTarget.getAttribute('href');
       });
 
---
+++ src/js/utils/storage.js
@@ -0,0 +1,48 @@
+const ARTICLES_READ_LS = 'articlesRead';
+const ARTICLE_LIFE_DAY = 1;
+const ARTICLE_LIFE_MS = ARTICLE_LIFE_DAY * 24 * 60 * 60 * 1000;
+
+export function setArticleRead(articleID) {
+  let articlesRead = localStorage.getItem(ARTICLES_READ_LS);
+  if (!articlesRead) {
+    articlesRead = {};
+  } else {
+    articlesRead = JSON.parse(articlesRead);
+  }
+  
+  if (articlesRead[articleID]) {
+    return;
+  }
+  articlesRead[articleID] = Date.now();
+  localStorage.setItem(ARTICLES_READ_LS, JSON.stringify(articlesRead));
+}
+
+function setArticlesRead(articles) {
+  localStorage.setItem(ARTICLES_READ_LS, JSON.stringify(articles));
+}
+
+export function getReadArticles() {
+  let articlesRead = localStorage.getItem(ARTICLES_READ_LS);
+  if (!articlesRead) {
+    articlesRead = {};
+  } else {
+    articlesRead = JSON.parse(articlesRead);
+  }
+  
+  // clear out old ones
+  let allKeys = Object.keys(articlesRead);
+  let articlesAltered = false;
+  for (let i = 0; i < allKeys.length; i++) {
+    // Delete anything old than TIME old
+    let value = articlesRead[allKeys[i]];
+    if (Date.now() - value > ARTICLE_LIFE_MS){
+      articlesAltered = true;
+      delete articlesRead[allKeys[i]];
+    }
+  }
+  if (articlesAltered) {
+    setArticlesRead(articlesRead);
+  }
+
+  return articlesRead;
+}
